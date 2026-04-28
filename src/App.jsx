42476c85099d4115922923e3aa1c115cc8299fb9
Mylenna Farias <mylennabrooks@gmail.com>
2026-04-28 11:33:55 -0400

    refactor: restructure HTML and CSS, add new components for themes and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,64 @@
 import { useState } from 'react'
 import './App.css'
 import { FormularioDeEvento } from './componentes/FormularioDeEvento'
+import { Tema } from './componentes/Tema'
+import { Banner } from './componentes/Banner'
 
 function App() {
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
+
   return (
     <main>
       <header>
         <img src="/tecboardlogo.png" alt='' />
       </header>
-      <section className='hero-section'>
-         <img src="/tecboardheader.png" alt=''/>
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]} />
+      </section>
+      <section>
+        <Tema tema={temas[1]} />
+      </section>
+      <section>
+        <Tema tema={temas[2]} />
+      </section>
+      <section>
+        <Tema tema={temas[3]} />
+      </section>
+      <section>
+        <Tema tema={temas[4]} />
+      </section>
+      <section>
+        <Tema tema={temas[5]} />
+      </section>
     </main>
   )
 }
 
 export default App
-  
